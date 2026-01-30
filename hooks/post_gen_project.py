5cb24411cce7e3ed286b45c5725d40f19ec8ab3c
jcook3701 <jcook3701+github@gmail.com>
2026-01-30 13:20:54 -0800

    Update template

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -21,7 +21,6 @@ import os
 
 from nutrimatic.core import make
 from nutrimatic.hooks.post_gen_logic import (
-    generate_cliff_changelog_dirs,
     generate_docs_templates,
     get_make_cmds,
 )
